0d2171c56baf5e8d5b185211f84445e30f54573c
Jovian Kurnia <jovian1gdf@gmail.com>
2022-12-31 03:37:22 +0700

    80% Progress
    - Fix Save and Load feature
    - Added Countdown/Timer feature
    - Fix merge error
    - Modify script for Journal and Map feature
    - Fixed player position
    - Modify Save System scripts

--- Assets/Scripts/Handlers/Game/Erutara/ErutaraOutroUIHandler.cs
+++ Assets/Scripts/Handlers/Game/Erutara/ErutaraOutroUIHandler.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class ErutaraOutroUIHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject outroObject;
+    public GameObject pauseObject;
+
+    public void ShowPause()
     {
-        
+        pauseObject.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ToResultScreen()
     {
-        
+        SceneManager.LoadScene("Result_Screen");
     }
 }
